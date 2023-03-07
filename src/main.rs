621f8d6a258c0566dcdd516910708ad7b014e6ca
seanmgarcia411 <sean.garcia411@gmail.com>
2023-03-06 21:28:01 -0600

    Making a small db, using lots of cool functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::path::Path;
+use std::{path::Path, collections::HashMap};
 
 #[macro_export]
 macro_rules! create_statement {
-    ($con:ident, $statement:expr, $($x:expr)*,) => {{
+    ($con:ident, $statement:expr, $($x:expr),*) => {{
         #[allow(unused_mut)]
         let mut statement = $con.prepare($statement).expect("Unable to prepare statement");
         let mut _i = 1usize;
@@ -19,16 +19,12 @@ fn main() {
     let connection = 
     if !Path::new("Database\\RefDB").exists() {
         let connection = sqlite::open("Database\\RefDB").unwrap();
-
-        let query = "
-            CREATE TABLE Cards (CardName TEXT);
-            INSERT INTO Cards VALUES ('BlueInstant');
-            INSERT INTO Cards VALUES ('RedEnchantment');
-            INSERT INTO Cards VALUES ('WhiteSorcery');
-            INSERT INTO Cards VALUES ('BlackCreature');
-        ";
-        connection.execute(query).unwrap();
-
+        create_table("Cards", &connection);
+        add_single_card("BlueInstant", &connection);
+        add_single_card("RedEnchantment", &connection);
+        add_single_card("WhiteSorcery", &connection);
+        add_single_card("BlackCreature", &connection);
+        
         connection
     }
     else {
@@ -36,7 +32,37 @@ fn main() {
 
         connection
     };
+    list_all_cards(&connection);
+}
 
-    let selcon = create_statement!(connection, "SELECT CardName FROM Cards",);
-    selcon.map;
+fn list_all_cards(connection: &sqlite::Connection) {
+    let sel_con = "SELECT * FROM Cards";
+    connection
+    .iterate(sel_con, |pairs| {
+        for &(card_name, value) in pairs.iter() {
+            println!("{} = {}", card_name, value.unwrap());
+        }
+        true
+    })
+    .unwrap();
+}
+
+fn add_single_card(card_name: &str, connection: &sqlite::Connection) {
+    let ins_con = format!("INSERT INTO Cards VALUES (@card_name);");
+    let mut ins_statement = create_statement!(connection, ins_con, card_name);
+    execute_update(ins_statement);
+}
+
+fn create_table(table_name: &str, connection: &sqlite::Connection) {
+    let table_con = format!("CREATE TABLE {table_name} (card_name TEXT);");
+    let mut make_table_statement = create_statement!(connection, table_con,);
+    execute_update(make_table_statement);
+}
+
+/// Runs an update statement
+fn execute_update(mut stmt: sqlite::Statement) -> Result<(),()> {
+    if let sqlite::State::Done = stmt.next().unwrap() {
+        return Ok(());
+    }
+    Err(())
 }
