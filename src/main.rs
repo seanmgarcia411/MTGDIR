14d746190c02252e97cf8563d998627a3a178f9e
seanmgarcia411 <sean.garcia411@gmail.com>
2023-03-06 20:41:52 -0600

    Macro no work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use std::path::Path;
 
+#[macro_export]
+macro_rules! create_statement {
+    ($con:ident, $statement:expr, $($x:expr)*,) => {{
+        #[allow(unused_mut)]
+        let mut statement = $con.prepare($statement).expect("Unable to prepare statement");
+        let mut _i = 1usize;
+    $(
+        statement.bind((_i, $x)).unwrap();
+        _i +=1;
+    )*
+    statement
+    }};
+}
+
 fn main() {
     //make the initial small test db
     let connection = 
@@ -23,5 +37,6 @@ fn main() {
         connection
     };
 
-
+    let selcon = create_statement!(connection, "SELECT CardName FROM Cards",);
+    selcon.map;
 }
