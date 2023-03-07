2cb3a95c6b860cd1827fa72d42cc3384f74cf308
seanmgarcia411 <sean.garcia411@gmail.com>
2023-03-06 20:33:01 -0600

    A database is created!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    //make the initial small test db
+    let connection = 
+    if !Path::new("Database\\RefDB").exists() {
+        let connection = sqlite::open("Database\\RefDB").unwrap();
+
+        let query = "
+            CREATE TABLE Cards (CardName TEXT);
+            INSERT INTO Cards VALUES ('BlueInstant');
+            INSERT INTO Cards VALUES ('RedEnchantment');
+            INSERT INTO Cards VALUES ('WhiteSorcery');
+            INSERT INTO Cards VALUES ('BlackCreature');
+        ";
+        connection.execute(query).unwrap();
+
+        connection
+    }
+    else {
+        let connection = sqlite::open("Database\\RefDB").unwrap();
+
+        connection
+    };
+
+
 }
